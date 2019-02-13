adfd8386ec53c6f3e5157b4063477d3720dabf04
Juan Minnocci <juan@monoqi.de>
2019-02-14 00:17:20 +0100

    Fetch initial data with loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch(handleInitialData())
+  }
 
   render() {
     return (
       <div>
-        Hello there!
+        {this.props.loading === true
+          ? <div>Loading...</div>
+          : <div>Loaded!</div>
+        }
       </div>
     )
   }
   
 }
 
-export default App
+function mapStateToProps ({ questions }) {
+  return {
+    loading: questions === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
